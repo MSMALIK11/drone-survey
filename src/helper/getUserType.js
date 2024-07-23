a0d8def4c94de3b425641594aab3fa6ababc2683
unknown <asingh1952306@gmail.com>
2024-07-23 14:06:47 +0530

    Fix owner

--- src/helper/getUserType.js
+++ src/helper/getUserType.js
@@ -1,11 +1,14 @@
 export const getUserType=(data)=>{
     if(!data) return null
     let type=""
-    if(data.is_admin){
-        type="Admin"
+    if(data.is_admin && data.is_owner){
+        type="Owner"
     }else if (data.is_owner){
         type="Owner"
-    }else{
+    }else if(data.is_admin ){
+        type="Admin"
+    }
+    else{
         type="Member"
     }
 
