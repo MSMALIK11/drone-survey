41f6b15a5c05b2251f66c24cc7cfa2592dc9dec7
unknown <asingh1952306@gmail.com>
2024-07-29 23:35:02 +0530

    Change  main  branch  to develop branch

--- src/protectedRoute/PublicRoutes.jsx
+++ src/protectedRoute/PublicRoutes.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React from "react";
+import { Navigate } from "react-router-dom";
 
 const PublicRoute = ({ children }) => {
-  const isUserExist = JSON.parse(localStorage.getItem('auth-user'));
+  const isUserExist = JSON.parse(localStorage.getItem("auth-user"));
 
   if (isUserExist) {
     return <Navigate to="/" replace />;
